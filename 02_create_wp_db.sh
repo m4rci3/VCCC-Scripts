5892566464a4a7e1383747a39bb212f94c6a2b14
Marcie <159983946+m4rci3@users.noreply.github.com>
2025-05-08 23:39:35 -0400

    Create 02_create_wp_db.sh
    This is part 2 of the script , it will prompt for the db root password you used during the secure install, name of the database that you want to create, the WordPress admin username that you want to give and the password that you want to use for that user. At the end of this it will read back what inputs you gave it.

---
+++ 02_create_wp_db.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+cat << "EOF"
+    ____             __     ___                     _       __
+   / __ \____ ______/ /_   |__ \    _______________(_)___  / /_
+  / /_/ / __ `/ ___/ __/   __/ /   / ___/ ___/ ___/ / __ \/ __/
+ / ____/ /_/ / /  / /_    / __/   (__  ) /__/ /  / / /_/ / /_
+/_/    \__,_/_/   \__/   /____/  /____/\___/_/  /_/ .___/\__/
+                                                 /_/
+
+EOF
+#!/bin/bash
+set -e
+exec > >(tee -a 02_db_setup.log) 2>&1
+
+# Prompt for DB root password
+read -s -p "Enter the MariaDB root password: " dbrootpass
+echo
+
+# Prompt for new WordPress DB, user, and password
+read -p "Enter name for new WordPress database: " wp_db
+read -p "Enter username for WordPress database user: " wp_user
+read -s -p "Enter password for WordPress database user: " wp_pass
+echo
+
+# Create DB and user
+mysql -u root -p"$dbrootpass" <<EOF
+CREATE DATABASE $wp_db;
+CREATE USER '$wp_user'@'localhost' IDENTIFIED BY '$wp_pass';
+GRANT ALL PRIVILEGES ON $wp_db.* TO '$wp_user'@'localhost';
+FLUSH PRIVILEGES;
+EOF
+
+echo "Database setup complete."
+echo "Created database:      $wp_db"
+echo "Created user:          $wp_user"
+echo "Password (hidden):     [REDACTED]"
+echo "Database setup complete. Now run ./03_setup_php_wordpress.sh"
